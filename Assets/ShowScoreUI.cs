85c4f32ebe043d3b9649ca17020f01cf78fb65c0
Anthony Kiniyalocts <kiniyaloca1@gmail.com>
2020-04-25 15:22:20 -0400

    initial commit

---
+++ Assets/ShowScoreUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowScoreUI : MonoBehaviour
+{
+
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        scoreText.text = "NICE SHOT!!!!!!!!!!!!!!!!!!!!!!!";
+    }
+}
